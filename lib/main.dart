f9eda088ad0e66d94e455a80cfb3aef7e10a4ca2
Ronaldo Pace <ronaldopace@gmail.com>
2019-12-10 22:08:46 +0100

    Removed test stuff unrelated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chess/game_bloc.dart';
 import 'package:flutter_chess/game_state.dart';
-import 'package:flutter_chess/sidekick_test.dart';
 import 'package:flutter_chess/widget_chess_board.dart';
 import 'package:provider/provider.dart';
 
@@ -16,24 +15,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         fontFamily: 'RobotoRegular',
       ),
       home: Scaffold(
-        body : SideKickTest())
-/*          body: Provider(
+          body: Provider(
               builder: (context) => GameBloc(),
               dispose: (context, GameBloc value) => value.onDispose(),
-              child: WidgetChessBoard())),*/
+              child: WidgetChessBoard())),
     );
   }
 }
